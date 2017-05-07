44b1c36ed9e514da7aed1bcdb6ff3b425d230c8b
Saurabh619 <nandwana.saurabh619@gmail.com>
2017-05-08 02:07:11 +0530

    added modules

---
+++ frontend/src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('sparsh')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ frontend/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('sparsh', ['ngCookies', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
